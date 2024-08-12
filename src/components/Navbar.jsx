bfc169b5853f206b8a7043c17fee9f88a2e1b1c4
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-12 21:08:05 +0800

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import { navLists } from "../constants";
 
 const Navbar = () => {
   return (
-    <header className="flex w-full items-center justify-between px-5 py-5 sm:px-10">
-      <img src={appleImg} alt="apple logo" width={20} height={20} />
-      <nav className="screen-max-width flex w-full">
+    <header className="flex w-full items-center px-5 py-5 sm:px-10">
+      <nav className="screen-max-width flex w-full justify-between">
         {/* Apple Logo */}
+        <img src={appleImg} alt="apple logo" width={14} height={18} />
 
         {/* Navbar Items  */}
-        <div className="flex justify-center max-sm:hidden">
+        <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((item, index) => (
             <div
               key={index}
@@ -20,12 +20,13 @@ const Navbar = () => {
             </div>
           ))}
         </div>
+
+        {/* Icons */}
+        <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
+          <img src={searchImg} alt="search" height={20} width={20} />
+          <img src={bagImg} alt="cart" height={20} width={20} />
+        </div>
       </nav>
-      {/* Icons */}
-      <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
-        <img src={searchImg} alt="search" height={20} width={20} />
-        <img src={bagImg} alt="cart" height={20} width={20} />
-      </div>
     </header>
   );
 };
