f183411ac65d651f2d8685661b95e84a25c0820f
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-12 16:26:16 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import React from "react";
-import {
-  Navbar,
-  Hero,
-  Features,
-  Testimonials,
-  CTA,
-  Footer,
-} from "./components";
-import "./App.css";
+import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <div className="mx-auto max-w-6xl space-y-12">
-        <Hero />
-        <Features />
-        <Testimonials />
-      </div>
-      <CTA />
-      <Footer />
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Navbar from "./Navbar";
-import Hero from "./Hero";
-import Features from "./Features";
-import Testimonials from "./Testimonials";
-import CTA from "./CTA";
-import Footer from "./Footer";
-
-export { Navbar, Hero, Features, Testimonials, CTA, Footer };
