0da997c6f15cddd5bf2ac6e463c139f4a4dd8953
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-12 13:41:21 +0800

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import {
-  Navbar,
-  Hero,
-  Features,
-  Testimonials,
-  CTA,
-  Footer,
-} from "./components";
-import "./App.css";
+  GsapFrom,
+  GsapFromTo,
+  GsapScrollTrigger,
+  GsapStagger,
+  GsapText,
+  GsapTimeline,
+  GsapTo,
+  Home,
+} from "./pages";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <div className="mx-auto max-w-6xl space-y-12">
-        <Hero />
-        <Features />
-        <Testimonials />
-      </div>
-      <CTA />
-      <Footer />
+    <div className="bg-black min-h-screen w-full">
+      <Router>
+        <Routes>
+          <Route path="/gsapto" element={<GsapTo />} />
+          <Route path="/gsapfrom" element={<GsapFrom />} />
+          <Route path="/gsapfromto" element={<GsapFromTo />} />
+          <Route path="/gsaptimeline" element={<GsapTimeline />} />
+          <Route path="/gsapstagger" element={<GsapStagger />} />
+          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
+          <Route path="/gsaptext" element={<GsapText />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
