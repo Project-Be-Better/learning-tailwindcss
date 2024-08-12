96778190380cbdf35b5edbe6f8decb62945d0fd6
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-12 20:59:33 +0800

    navbar intermediate commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { appleImg } from "../utils";
+import { appleImg, searchImg, bagImg } from "../utils";
+import { navLists } from "../constants";
 
 const Navbar = () => {
   return (
-    <header>
-      <nav>
-        <img src={appleImg} alt="apple image" />
+    <header className="flex w-full items-center justify-between px-5 py-5 sm:px-10">
+      <img src={appleImg} alt="apple logo" width={20} height={20} />
+      <nav className="screen-max-width flex w-full">
+        {/* Apple Logo */}
+
+        {/* Navbar Items  */}
+        <div className="flex justify-center max-sm:hidden">
+          {navLists.map((item, index) => (
+            <div
+              key={index}
+              className="cursor-pointer px-5 text-gray transition-all hover:text-white"
+            >
+              {item}
+            </div>
+          ))}
+        </div>
       </nav>
+      {/* Icons */}
+      <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
+        <img src={searchImg} alt="search" height={20} width={20} />
+        <img src={bagImg} alt="cart" height={20} width={20} />
+      </div>
     </header>
   );
 };
