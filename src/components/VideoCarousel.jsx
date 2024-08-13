4d8896d5a3f6c71c2d10557e5664f9c48be1b086
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-14 00:23:52 +0800

    20240813-video-carousel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { hightlightsSlides } from "../constants";
 
 const VideoCarousel = () => {
-  return <div>VideoCarousel</div>;
+  // Create References
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
+
+  // Destructure the State
+  const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((slide, i) => (
+          <div id="slider" key={i} className="pr-10 sm:pr-20">
+            <div className="video-carousel_container">
+              <div className="flex-center h-full w-full overflow-hidden rounded-3xl bg-black">
+                <video
+                  id="video"
+                  preload="auto"
+                  autoPlay
+                  muted
+                  playsInline={true}
+                  ref={(el) => (videoRef.current[i] = el)}
+                  onPlay={() => {
+                    setVideo((prev) => ({ ...prev, isPlaying: true }));
+                  }}
+                >
+                  <source src={slide.video} type="video/mp4" />
+                </video>
+              </div>
+
+              <div className="absolute left-[5%] top-12 z-10">
+                {slide.textLists.map((text, i) => (
+                  <p key={i} className="text-xl font-thin md:text-2xl">
+                    {text}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default VideoCarousel;
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Navbar from "./Navbar";
 import Highlights from "./Highlights";
 import Hero from "./Hero";
 import VideoCarousel from "./VideoCarousel";
+
 export { Hero, Navbar, Highlights, VideoCarousel };
